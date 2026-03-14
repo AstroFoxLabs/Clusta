da1ced80a7d2947d07c3d7566b38ba06908167c9
AstroFox <astrofox@astrofoxlabs.com>
2026-03-14 13:34:48 +0100

    Removed creating default data functionality from render bootstrap

--- src/render/composables/useAppBootstrap.ts
+++ src/render/composables/useAppBootstrap.ts
@@ -4,8 +4,6 @@ import { useImageStore } from '@render/stores/imageStore';
 import { useNotificationStore } from '@render/stores/notificationStore';
 import { useSettingsStore } from '@render/stores/settingsStore';
 import { useTagStore } from '@render/stores/tagStore';
-import { getFileBufferFromPath } from '@render/utils';
-import { ImageFilePayload } from '@shared/types';
 
 // Closure to make sure the stores are available before the application is fully loaded
 export const useAppBootstrap = async () => {
@@ -23,7 +21,6 @@ export const useAppBootstrap = async () => {
             await categoryStore.fetchAll();
             await tagStore.fetchAll();
             await excalidrawStore.fetchAllRecords();
-            await initializeDefaultData();
 
             if (excalidrawStore.scenes.length >= 0) {
                 await excalidrawStore.selectScene(excalidrawStore.scenes[0]);
@@ -42,60 +39,6 @@ export const useAppBootstrap = async () => {
             notificationStore.addEventMessage('Failed to initialize app. Check Logs or contact Developer.');
             console.error('Error during app initialization:', error);
             throw error;
-        }
-    };
-
-    const initializeDefaultData = async (): Promise<void> => {
-        // initialize default image
-        let iID = '';
-        let cID = '';
-        let tID = '';
-        try {
-            if (imageStore.collection.length === 0) {
-                const buffer = await getFileBufferFromPath('src/render/public/assets/default.webp');
-                const payload: ImageFilePayload = {
-                    name: 'cutefox',
-                    size: buffer.length,
-                    data: buffer,
-                    hash: 'defaulthashwithcutefox',
-                    mimeType: 'image/webp',
-                };
-
-                await imageStore.create(payload.name, payload);
-            }
-        } catch (error) {
-            console.error('Failed to initialize default image:', error);
-            throw error;
-        }
-
-        // initialize default category
-        try {
-            if (categoryStore.collection.length === 0) {
-                await categoryStore.create('Your Category');
-            }
-        } catch (error) {
-            console.error('Failed to initialize default category:', error);
-            throw error;
-        }
-
-        // initialize default tag
-        try {
-            if (tagStore.collection.length === 0) {
-                await tagStore.create('Your Tag');
-            }
-        } catch (error) {
-            console.error('Failed to initialize default tag:', error);
-            throw error;
-        }
-
-        // assign default category and tag to default image
-        try {
-            if (iID && cID) {
-                await categoryStore.assignToImage(iID, cID);
-                await tagStore.assignToImage(iID, tID);
-            }
-        } catch (error) {
-            console.error('Failed to assign default category and tag to default image:', error);
         }
     };
 
